31057dfe5e345709fba20f229180360935ff0abe
Reed <bobbyreed@ou.edu>
2021-08-06 13:59:23 -0500

    GameJam session updates

--- Scenes/EdgeRoom.gd
+++ Scenes/EdgeRoom.gd
@@ -1,16 +1,24 @@
 extends Spatial
 
+signal trackchangeCollide_onEnter
+
 onready var chillMusic = $ChillTheme
 onready var creepyMusic = $CreepyTheme
-onready var trackchangeCollide = $DoorwayMusicChange
+onready var trackchangeBody = $DoorwayMusicChange
+onready var trackchangeCollide = $DoorwayMusicChange/CollisionShape
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	
 func _physics_process(delta):
+	pass
 	
-
+func _on_character_body_enter(body):
+	print("Collision")
+	if (body.get_name() == "Player"):
+		print("Ouch!")
+	
 func _trackChange():
 	if chillMusic.stream_paused:
 		chillMusic.stop()
@@ -18,3 +26,6 @@ func _trackChange():
 	elif not chillMusic.stream_paused:
 		creepyMusic.stop()
 		chillMusic.play()
+
+func _on_EdgeRoom_trackchangeCollide_onEnter():
+	_trackChange()
