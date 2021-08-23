fbfc1fa7e22f18620485d44230e00ac3a5fa1fff
John Grime <jgrime@ou.edu>
2021-08-23 10:30:16 -0500

    Moved Brett node out of the navigation tree, added navigation node path to exported Brett variables

--- Scenes/Brett.gd
+++ Scenes/Brett.gd
@@ -1,19 +1,30 @@
 extends KinematicBody
 
-var path = []
-var path_node_index = 0
-var player = null
-
 # Allow to be changed in the editor for convienence
 export var speed = 90
 export var player_path = "../../JaneDough"
+export var navigation_path = "../../Navigation"
 
-onready var nav = get_parent()
+var path = []
+var path_node_index = 0
+
+var nav = null
+var player = null
 
 func _ready():
+	# Try to find specified player node
 	player = get_node(player_path)
 	if player == null:
-		print("Problem getting player object from path '%s'" % player_path)
+		print('Problem getting player node from path "%s"' % player_path)
+		if !(player is KinematicBody):
+			print('Warning; player node (%s") is not type KinematicBody!' % player_path)
+
+	# Try to find specified navigation node
+	nav = get_node(navigation_path)
+	if nav == null:
+		print("Problem getting navigation node from path '%s'" % navigation_path)
+		if !(nav is Navigation):
+			print('Warning; navigation node (%s") is not type Navigation!' % navigation_path)
 
 func _physics_process(delta):
 	if path_node_index < path.size():
